278deda512cf09eac57add4e192a88fd6a7ca0d7
Guillaume Desjardins <guillaume.desjardins@gmail.com>
2013-05-14 17:52:26 -0400

    BUG FIX: DBN lower bound estimation. Entropy of posterior distribution for ultimate layer should not be counted, when its been marginalized out in the last-layer RBM.
    Also first pass (buggy) implementation of estimator proposed by Lucas Theis.

--- scripts/likelihood/dbn_tools.py
+++ scripts/likelihood/dbn_tools.py
@@ -17,8 +17,8 @@ logging.basicConfig(level=logging.INFO)
 def xent(p, k):
     return -k * numpy.log(p+1e-5) - (1. - k ) * numpy.log(1-p+1e-5)
 
-def bernouilli(rbm, mean):
-    return (rbm.rng.random_sample(mean.shape) < mean).astype(floatX)
+def bernouilli(rng, mean):
+    return (rng.random_sample(mean.shape) < mean).astype(floatX)
 
 def compute_likelihood_lbound(model, log_z, test_x, M=5):
     """
@@ -47,6 +47,7 @@ def compute_likelihood_lbound(model, log_z, test_x, M=5):
     --------
     Scalar, representing negative log-likelihood of test data under the model.
     """
+
     i = 0.
     eq_log_p = 0
     for i in xrange(0, len(test_x), model.batch_size):
@@ -63,26 +64,26 @@ def compute_likelihood_lbound(model, log_z, test_x, M=5):
 
         # Estimate E_q[ln p(h_{L-1})]
         for j in xrange(M):
-            h_lm1_mean = posmfs[-2]
-            h_lm1_sample = bernouilli(model.rbms[-1], h_lm1_mean)
-            _eq_log_px += -model.rbms[-1].fe_v_func(h_lm1_sample)
 
-        # Estimate E_q[ ln p(h_i | h_{i+1}) ], for all i < L-1
-        for rbm, hi_mean, hip1_mean in zip(model.rbms[:-1], posmfs[:-2], posmfs[1:-1]):
-            for j in xrange(M):
-                # draw sample from Q(h_i | h_{i-1})
-                hi_sample = bernouilli(rbm, hi_mean)
-                # Compute likelihood contribution from log p(h_i|h_{i+1})]
-                p_hi_given_hip1 = rbm.v_given_h_func(hip1_mean)
-                _eq_log_px += -xent(p_hi_given_hip1, hi_sample).sum(axis=1)
+            # Generate samples from mean-field distributions
+            samples = [posmfs[0]]
+            for i in range(1,len(posmfs)):
+                samples += [bernouilli(model.rbms[i-1].rng, posmfs[i])]
+
+            _eq_log_px += -model.rbms[-1].fe_v_func(samples[-2])
+
+            # Estimate E_q[ ln p(h_i | h_{i+1}) ], for all i < L-1
+            for rbm, hi, hip1 in zip(model.rbms[:-1], samples[:-2], samples[1:-1]):
+                p_hi_given_hip1 = rbm.v_given_h_func(hip1)
+                _eq_log_px += -xent(p_hi_given_hip1, hi).sum(axis=1)
             
         eq_log_px = _eq_log_px / M
 
-        # Contribution of the entropy of h(q) to the variational lower bound
+        # Contribution of the entropy of h(q) to the variational lower bound.
+        # Do not count entropy of p(h_L | h_{L-1}), since h_L has been marginalized.
         hq = 0
-        for posmf in posmfs[1:]:
-            temp = xent(posmf, posmf)
-            hq += numpy.sum(temp, axis=1)
+        for posmf in posmfs[1:-1]:
+            hq += xent(posmf, posmf).sum(axis=1)
         eq_log_px += hq
 
         # perform moving average of negative likelihood
@@ -92,3 +93,39 @@ def compute_likelihood_lbound(model, log_z, test_x, M=5):
     eq_log_p -= log_z
 
     return eq_log_p
+
+def logmean(x, axis=0):
+    return numpy.log(numpy.exp(x - x.max()).mean(axis=axis)) + x.max()
+
+def compute_likelihood_lbound_theis(model, log_z, test_x, M=5):
+    """
+    See Figure 4 of "In all likelihood, deep belief is not enough", Theis et al.
+    """
+
+    i = 0.
+    ulogprob = 0
+
+    for i in xrange(0, len(test_x), model.batch_size):
+
+        # recast data as floatX and apply preprocessing if required
+        x = numpy.array(test_x[i:i + model.batch_size, :], dtype=floatX)
+
+        logiws = numpy.zeros((M, model.batch_size)) 
+
+        for j in xrange(M):
+            sample = x
+            for rbm in model.rbms[:-1]:
+                logiws[j, :] += -rbm.fe_v_func(sample)
+                sample = rbm.sample_h_given_v_func(sample)
+                logiws[j, :] += -rbm.fe_h_func(sample)
+            logiws[j, :] += model.rbms[-1].fe_v_func(sample)
+
+        ulogprob_x = logmean(logiws, axis=0)
+
+        # perform moving average of negative likelihood
+        # divide by len(x) and not bufsize, since last buffer might be smaller
+        ulogprob = (i * ulogprob + ulogprob_x.sum()) / (i + len(x))
+
+    logprob = ulogprob - log_z
+
+    return logprob
