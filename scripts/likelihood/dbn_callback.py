553c48ab7991369c8fddefbcaabc1412115f0b90
Guillaume Desjardins <guillaume.desjardins@gmail.com>
2013-05-16 17:31:27 -0400

    BUG FIX: logz is now a shared variable which auto-resets on batch_train_func

--- scripts/likelihood/dbn_callback.py
+++ scripts/likelihood/dbn_callback.py
@@ -6,17 +6,21 @@ from deep_tempering.scripts.likelihood import rbm_tools
 from deep_tempering.scripts.likelihood import dbn_tools
 from deep_tempering.tempered_dbn import TemperedDBN
 from deep_tempering.utils import logging
+floatX = theano.config.floatX
+npy_floatX = getattr(numpy, floatX)
 
 class pylearn2_dbn_likelihood_callback(TrainingCallback):
 
-    def __init__(self, trainset, interval=10):
+    def __init__(self, trainset, interval=10, layer=-1):
         """
         :param trainset: pylearn2 dataset object.
         :param interval: run callback every `this` number of iterations
         """
         self.trainset = trainset
         self.interval = interval
-        self.logger = logging.HDF5Logger('dbn_likelihood_callback.hdf5')
+        self.layer = layer
+        fname = 'dbn%i_%s_callback.hdf5' % (layer, trainset.which_set)
+        self.logger = logging.HDF5Logger(fname)
 
         self.jobman_results = {
                 'best_batches_seen': 0,
@@ -27,23 +31,30 @@ class pylearn2_dbn_likelihood_callback(TrainingCallback):
                 }
 
     def __call__(self, model, train, algorithm):
+        assert self.layer < len(model.rbms)
+        if model.batches_seen == 0:
+            return
         if (model.batches_seen % self.interval) != 0:
             return
 
-        rbm = model.rbms[-1]
+        rbm = model.rbms[self.layer]
 
-        if (rbm.n_h <= 25):
-            (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_h_func), 0.
-        elif (rbm.n_v <= 25):
-            (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_v_func), 0.
-        else:
-            (logz, var_logz), _aisobj = rbm_tools.rbm_ais(
-                    rbm.get_uncentered_param_values(),
-                    n_runs=100)
+        logz = rbm.logz.get_value()
+        if not logz:
+            if (rbm.n_h <= 25):
+                (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_h_func), 0.
+            elif (rbm.n_v <= 25):
+                (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_v_func), 0.
+            else:
+                (logz, var_logz), _aisobj = rbm_tools.rbm_ais(
+                        rbm.get_uncentered_param_values(),
+                        n_runs=100)
+            rbm.logz.set_value(npy_floatX(logz))
+            rbm.var_logz = var_logz
 
         train_ll = dbn_tools.compute_likelihood_lbound(model, logz, self.trainset.X)
 
-        self.log(model, train_ll, logz, var_logz)
+        self.log(model, train_ll, logz, rbm.var_logz)
         if model.jobman_channel:
             model.jobman_channel.save()
 
--- scripts/likelihood/rbm_callback.py
+++ scripts/likelihood/rbm_callback.py
@@ -1,3 +1,4 @@
+import theano
 import numpy
 import pickle
 
@@ -5,6 +6,8 @@ from pylearn2.training_callbacks.training_callback import TrainingCallback
 from deep_tempering.scripts.likelihood import rbm_tools
 from deep_tempering.tempered_dbn import TemperedDBN
 from deep_tempering.utils import logging
+floatX = theano.config.floatX
+npy_floatX = getattr(numpy, floatX)
 
 class pylearn2_rbm_likelihood_callback(TrainingCallback):
 
@@ -12,7 +15,8 @@ class pylearn2_rbm_likelihood_callback(TrainingCallback):
         self.trainset = trainset
         self.interval = interval
         self.layer = layer
-        self.logger = logging.HDF5Logger('rbm%i_likelihood_callback.hdf5' % layer)
+        fname = 'rbm%i_%s_callback.hdf5' % (layer, trainset.which_set)
+        self.logger = logging.HDF5Logger(fname)
 
         self.jobman_results = {
                 'best_batches_seen': 0,
@@ -28,25 +32,33 @@ class pylearn2_rbm_likelihood_callback(TrainingCallback):
             """
             Helper function which generates the representation at layer `self.layer`.
             """
-            for rbm in model.rbms[:self.layer]:
-                x = rbm.post_func(x)
+            if isinstance(model, TemperedDBN):
+                for rbm in model.rbms[:self.layer]:
+                    x = rbm.post_func(x)
             return x
 
+        if model.batches_seen == 0:
+            return
         if (model.batches_seen % self.interval) != 0:
             return
-        if isinstance(model, TemperedDBN):
-            rbm = model.rbms[self.layer]
 
-        if (rbm.n_h <= 25):
-            (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_h_func), 0.
-        elif (rbm.n_v <= 25):
-            (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_v_func), 0.
-        else:
-            (logz, var_logz), _aisobj = rbm_tools.rbm_ais(
-                    rbm.get_uncentered_param_values(),
-                    n_runs=100,
-                    data = self.trainset.X,
-                    preproc = preproc)
+        rbm = model.rbms[self.layer] if isinstance(model, TemperedDBN) else model
+
+        logz = rbm.logz.get_value()
+        if not logz:
+            # Estimate partition function
+            if (rbm.n_h <= 25):
+                (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_h_func), 0.
+            elif (rbm.n_v <= 25):
+                (logz, var_logz) = rbm_tools.compute_log_z(rbm, rbm.fe_v_func), 0.
+            else:
+                (logz, var_logz), _aisobj = rbm_tools.rbm_ais(
+                        rbm.get_uncentered_param_values(),
+                        n_runs=100,
+                        data = self.trainset.X,
+                        preproc = preproc)
+            rbm.logz.set_value(npy_floatX(logz))
+            rbm.var_logz = var_logz
 
         train_ll = rbm_tools.compute_likelihood(rbm,
                 data = self.trainset.X,
@@ -54,7 +66,7 @@ class pylearn2_rbm_likelihood_callback(TrainingCallback):
                 free_energy_fn = rbm.fe_v_func,
                 preproc = preproc)
 
-        self.log(rbm, train_ll, logz, var_logz)
+        self.log(rbm, train_ll, logz, rbm.var_logz)
         if model.jobman_channel:
             model.jobman_channel.save()
 
