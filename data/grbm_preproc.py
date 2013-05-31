a840c9c730f0ae54e002ec6e9eec8117ecd3a527
Guillaume Desjardins <guillaume.desjardins@gmail.com>
2013-05-30 20:09:28 -0400

    BUG FIX: run preprocessor on GPU * Added mean to the GRBM reconstruction

--- data/grbm_preproc.py
+++ data/grbm_preproc.py
@@ -10,6 +10,7 @@ import theano.tensor as T
 
 from pylearn2.base import Block
 from scipy.io import loadmat
+floatX = theano.config.floatX
 
 class GRBMPreprocessor(object):
 
@@ -23,16 +24,16 @@ class GRBMPreprocessor(object):
     def load_params(self):
         # load GRBM parameters
         params = loadmat(self.fname_best)
-        self.Wv = params['vishid_all']
-        self.hbias = params['hidbiases_all'][0]
-        self.vbias = params['visbiases'][0]
+        self.Wv = params['vishid_all'].astype(floatX)
+        self.hbias = params['hidbiases_all'][0].astype(floatX)
+        self.vbias = params['visbiases'][0].astype(floatX)
         self.n_v = len(self.vbias)
         self.n_h = len(self.hbias)
         # load statistics for reconstruction
         means = loadmat(self.fname_means)
         self.zz = numpy.hstack(
-                (means['zz1'] * numpy.ones(self.n_v/2),
-                 means['zz2'] * numpy.ones(self.n_v/2)))
+                (means['zz1'] * numpy.ones(self.n_v/2, dtype=floatX),
+                 means['zz2'] * numpy.ones(self.n_v/2, dtype=floatX)))
         self.vmean = numpy.hstack((means['aa1'], means['aa2']))
 
     def do_theano(self):
@@ -41,7 +42,7 @@ class GRBMPreprocessor(object):
         self.h_given_x_func = theano.function([x], h)
 
     def preproc(self, xval):
-        return self.h_given_x_func(xval)
+        return self.h_given_x_func(xval.astype(floatX))
 
     def reconstruct(self, hval):
         v = numpy.dot(hval, self.Wv.T) + self.vbias
